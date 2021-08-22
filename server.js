2a6f86582353be5cc5f5481d8afff1d0878054d1
Nyjia Scott <nkscott97@gmail.com>
2021-08-22 16:27:00 -0400

    changed my connect method

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 // Connect to DB //
-// 'mongodb://localhost:27017/diary-db'
-mongoose.connect("mongodb://Nyjiakscott97:TJWUa2nNlY4ypeBt@cluster0-shard-00-00.tvlbm.mongodb.net:27017,cluster0-shard-00-01.tvlbm.mongodb.net:27017,cluster0-shard-00-02.tvlbm.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-142zxk-shard-0&authSource=admin&retryWrites=true&w=majority",
+
+mongoose.connect('mongodb://localhost:27017/diary-db',
 
     {
         useNewUrlParser: true,
