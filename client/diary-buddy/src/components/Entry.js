665a0df1637c4f800a0a7e70eed6c4859197a042
Nyjia Scott <nkscott97@gmail.com>
2021-08-22 01:23:58 -0400

    changes made to App.js and Entry.js

--- client/diary-buddy/src/components/Entry.js
+++ client/diary-buddy/src/components/Entry.js
@@ -1,14 +1,41 @@
-import React from "react"
+import React, {useState} from "react"
+import AddEntryForm from "./AddEntryForm"
 
 function Entry(props) {
-    const {title, _id} = props
+    const {date, location, entry, image, mood, _id} = props
+    const [editToggle, setEditToggle] = useState(false)
+    
     return(
         <div className="entry">
-            <h1>Title: {title}</h1>
-            <p></p>
-            <button className="delete-btn" onClick={() => props.deleteEntry(_id)}>
-                Delete
-            </button>       
+            {!editToggle ?
+                <>
+                    <p>Date: {date}</p>
+                    <p>Location: {location}</p>
+                    <p>Entry: {entry}</p>
+                    <p>Image: {image}</p>
+                    <p>Mood: {mood}</p>
+                    <button className="delete-btn" onClick={() => props.deleteEntry(_id)}>
+                        Delete
+                    </button> 
+                    <button className="edit-btn" onClick={() => setEditToggle(prevToggle => !prevToggle)}>
+                        Edit
+                    </button>
+                </>
+            :  
+                <>
+                    <AddEntryForm 
+                        date={date}
+                        location={location}
+                        entry={entry}
+                        image={image}
+                        mood={mood}
+                        _id={_id}
+                        btnText="Submit Edit"
+                        submit={props.editEntry}
+                    /> 
+                    <button>Close</button>
+                </>
+            }   
         </div>
     )
 }
