3b42d6786bfa568bb470718fd50b259e84c64a97
James Caleca <jacaleca2@gmail.com>
2021-08-21 18:45:22 -0600

    Created router and schema for new blank entry, as well as HTML file in src folder

---
+++ routes/entryRouter.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const entryRouter = express.Router()
+const Entry = require('../models/entry.js')
+
+// Get All //
+entryRouter.get('/', (req, res, next) => {
+    Entry.find((err, entries) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(entries)
+    })
+})
+
+// Post One //
+entryRouter.post('/', (req, res, next) => {
+    const newEntry = new Entry(req.body)
+    newEntry.save((err, savedEntry) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(savedEntry)
+    })
+})
+
+// Delete One //
+entryRouter.delete('/:entryId', (req, res, next) => {
+    Entry.findOneAndDelete({ _id: req.params.entryId }, (err, deletedItem) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(`Successfully deleted entry '${deletedItem.date}' from the database`)
+    })
+})
+
+// Put One //
+entryRouter.put('/:entryId', (req, res, next) => {
+    Entry.findOneAndUpdate(
+        { _id: req.params.entryId },
+        req.body,
+        { new: true },
+        (err, updatedEntry) => {
+            if(err){
+                res.status(500)
+                return next(err)
+            }
+            return res.status(201).send(updatedEntry)
+        }
+    )
+})
+
+module.exports = entryRouter
