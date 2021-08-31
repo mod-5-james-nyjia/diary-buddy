5c448bc02c216af259605c3bd93727a132d47ee1
Nyjia Scott <nkscott97@gmail.com>
2021-08-31 09:42:48 -0400

    edit button is now working

--- client/diary-buddy/src/components/Entry.js
+++ client/diary-buddy/src/components/Entry.js
@@ -4,40 +4,29 @@ import "../styles.css"
 import { useEntries } from "../contexts/userEntryContext"
 
 function Entry(props) {
-    const {date, location, entry, isEntry, image, isImage, mood, _id, prompt, positive, negative, text } = props
+    const {date, location, entry, isEntry, image, mood, _id, prompt, positive, negative} = props
     const {deleteEntry, editEntry} = useEntries()
     const [editToggle, setEditToggle] = useState(false)
-
-    // positive / negative display on the entries page and the submit button on the home page,
 
     function toggle(){
         setEditToggle(prevToggle => !prevToggle)
     }
     console.log(props,"props")
-    // console.log(prompt, "prompt")
-    console.log(entry, "entry")
-    console.log(positive, "positive")
-    console.log(negative, "negative")
-    console.log(date)
-    console.log(mood)
-    
+    console.log(prompt, "prompt")
+
     return(
         <div className="entry">
             <p>{date === null ? <>{null}</> : <><b>Date:</b> {date}</> }</p>
-            {/* date is a string */}
             <p>{{location}.length > 0 ? <><b>Location:</b> {location}</> : <>{null}</>}</p>
-            {/* location is a string and is false*/}
             <div>{image === '' ? <>{null}</> : <img src={image} alt="img" className='entry-image'></img>}</div>
             <p>{mood === '' ? null : mood === undefined ? null : <><b>Mood</b>: {mood}</> }</p>
-            <p>{!isEntry ? <>Prompt: {prompt}</> : <><b>Entry:</b> {entry}</>}</p>
+            {/* <p>{!isEntry ? <>Prompt: {prompt}</> : <><b>Entry:</b> {entry}</>}</p> */}
+            <p>Entry: {entry}</p>
             <p>{{positive}.length > 0 ? <><b>Positives</b>: {positive}</> : <>{null}</>}</p>
             <p>{{negative}.length > 0 ? <><b>Negatives</b>: {negative}</> : <>{null}</>}</p>            
-
-            {/* <p>{text === "" ? null : <>Text: {text}</>}</p> */}
         
             <button className="delete-btn" onClick={() => deleteEntry(_id)}>              
                 Delete
-                {/* {console.log(deleteEntry(_id), "delete")} */}
             </button> 
             <button className="edit-btn" onClick={() => toggle()}>
                 Edit
@@ -46,6 +35,7 @@ function Entry(props) {
 
             {editToggle === true ?
                 <>
+                {/* return edit form instead */}
                     <AddEntryForm 
                         date={date}
                         location={location}
@@ -53,10 +43,14 @@ function Entry(props) {
                         image={image}
                         mood={mood}
                         _id={_id}
-                        btnText="Submit Edit"
+                        // btnText="Submit Edit"
                         submit={editEntry}
-                    /> 
-                    <button onClick={() => console.log(editEntry(), "EDit")}>Submit Edit</button>
+                    />
+                    {/* i know I'm getting the correct id. ID is not the problem. */}
+                    {/* inputs are working as they should */}
+                    {/* it's also not the editEntry function */}
+                    {/* handleChange is fine as well */}
+                    {/* seems to be saved in state but is not being reflected on the dom */}
                     <button onClick={() => toggle()}>Close</button>
                 </>
                 :  
--- client/diary-buddy/src/components/TemplateOne.js
+++ client/diary-buddy/src/components/TemplateOne.js
@@ -8,7 +8,9 @@ function TemplateOne(props) {
 
     const initTemplateInputs = 
     {
-        prompt: props.prompt || '',
+        // prompt: props.prompt || '',
+        entry: props.entry || '',
+
         positive: props.positive || '',
         negative: props.negative || '',
         image: props.image || '',
@@ -87,7 +89,7 @@ function TemplateOne(props) {
                         <h3 className='content-h3'>Journal Prompt</h3>
                         <p>{dailyPromptOne()}</p>
                         <textarea
-                            name='prompt'
+                            name='entry'
                             className='journal-prompt'
                             rows='10'
                             cols='40'
